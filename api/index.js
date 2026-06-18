bfba4ac14ac17d68fdb6e7f3a7b3c31d07a869da
Dania Shabih <faizan2311e@aptechsite.net>
2026-06-18 19:12:34 +0500

    .

--- api/index.js
+++ api/index.js
@@ -2,10 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const connectDB = require('./config/db');
-
 const app = express();
-connectDB();
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
