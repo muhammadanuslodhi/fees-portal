3df32ddceafb9424c14342cc2fc01daa5b0fc4a4
muhammadanuslodhi <anuslodhi786@gmail.com>
2026-07-18 05:57:06 -0700

    new

--- api/prisma.js
+++ api/prisma.js
@@ -1,18 +1,18 @@
 const { PrismaClient } = require('@prisma/client');
 
-// Ensure we don't create multiple instances in Vercel serverless environment
-let prisma;
-
-if (!global.prisma) {
-  global.prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL
-      }
-    }
-  });
+// Ensure DATABASE_URL is set before creating the client
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    'DATABASE_URL environment variable is not set. ' +
+    'Please add it to your Vercel project environment variables.'
+  );
 }
 
-prisma = global.prisma;
+// Ensure we don't create multiple instances in Vercel serverless environment
+if (!global.prisma) {
+  global.prisma = new PrismaClient();
+}
+
+const prisma = global.prisma;
 
 module.exports = prisma;
